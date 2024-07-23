f7aded09502474e632775fe91bc33616e2d921ca
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2024-07-23 23:21:22 +0700

    push

---
+++ BTVN_3/src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
+import MainContent from "./components/main-content/MainContent"
+
+import Nav from "./components/nav/Nav"
+
+
+const App = () => {
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    <MainContent/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
